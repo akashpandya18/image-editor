5db71f17ddd815300f9e2f0297d193dba0d99069
akashpandya <akash.pandya@mindinventory.com>
2023-03-23 11:54:49 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./styles/index.css";
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1 className='title'>Welcome to Image Editor</h1>
+    </div>
+  );
+};
+
+export default App;
